35598b3151de81d4d6da63b435759d41bc87c4a8
Geethanjali kotaru <geethanjalikotaru@my.unt.edu>
2023-09-14 14:45:04 -0500

    added cross origin

--- src/main/java/com/example/Backendservices/BackendservicesApplication.java
+++ src/main/java/com/example/Backendservices/BackendservicesApplication.java
@@ -5,12 +5,13 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @SpringBootApplication
 @RestController
 @RequestMapping("api/portfoliocheck")
